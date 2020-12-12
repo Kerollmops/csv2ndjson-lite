64966e20688ac1e8bc18899843a6feaf53ac6c3b
Clément Renault <clement@meilisearch.com>
2020-12-12 12:20:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# csv2ndjson-lite
+
+A little tool to convert a csv to a valid ndjson/json-stream (supported comma separated arrays).
+
+## Usage
+
+Here is an example CSV file, note that there is fields that are comma separated.
+
+```csv
+title,genres
+Carmencita,"Documentary,Short"
+Miss Jerry,Romance
+Corbett and Courtney Before the Kinetograph,"Short,Sport"
+```
+
+If you want to output an array when CSV values are comma separated, specify those headers as arguments.
+
+```bash
+cat mydata.csv | cargo run --release 'genres'
+```
+
+The output of the previous command would be something like so.
+
+```json
+{"title":"Carmencita","genres": ["Documentary","Short"] }
+{"title":"Miss Jerry","genres": ["Romance"]}
+{"title":"Corbett and Courtney Before the Kinetograph","genres":["Short","Sport"]}
+```
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+use std::{env, io};
+use std::error::Error;
+use json::{object::Object, JsonValue};
+
+fn try_main() -> Result<(), Box<dyn Error>> {
+    let array_fields: HashSet<_> = env::args().collect();
+
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    let headers = rdr.headers()?.clone();
+
+    for result in rdr.into_records() {
+        let record = result?;
+        let mut object = Object::new();
+
+        for (i, field) in record.iter().enumerate() {
+            let header = headers.get(i).unwrap();
+            let value = if array_fields.contains(header) {
+                JsonValue::Array(field.split(',').map(JsonValue::from).collect())
+            } else {
+                json::from(field)
+            };
+            object.insert(&header, value);
+        }
+
+        println!("{},", object.dump());
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = try_main() {
+        eprintln!("{}", e);
+    }
+}
