744c8114be4de0233349ee78537b695bd6a08900
Clément Renault <clement@meilisearch.com>
2021-05-30 12:00:29 +0200

    Add a StructOpt based argument parsing

--- README.md
+++ README.md
@@ -2,27 +2,35 @@
 
 A little tool to convert a csv to a valid ndjson/json-stream (supported comma separated arrays).
 
+## Installation
+
+You can use cargo to install this tool:
+
+```bash
+cargo install -f csv2ndjson-lite
+```
+
 ## Usage
 
 Here is an example CSV file, note that there is fields that are comma separated.
 
 ```csv
-title,genres
-Carmencita,"Documentary,Short"
-Miss Jerry,Romance
-Corbett and Courtney Before the Kinetograph,"Short,Sport"
+title,genres,scores,duration
+Carmencita,"Documentary,Short","23,25,56",3.5
+Miss Jerry,Romance,"3,2,6",5.3
+Corbett and Courtney Before the Kinetograph,"Short,Sport","203,250,506",
 ```
 
 If you want to output an array when CSV values are comma separated, specify those headers as arguments.
 
 ```bash
-cat mydata.csv | cargo run --release 'genres'
+cat mydata.csv | csv2ndjson-lite --arrays genres scores --numbers scores duration
 ```
 
 The output of the previous command would be something like so.
 
 ```json
-{"title":"Carmencita","genres":"Documentary,Short"}
-{"title":"Miss Jerry","genres":"Romance"}
-{"title":"Corbett and Courtney Before the Kinetograph","genres":"Short,Sport"}
+{"title":"Carmencita","genres":["Documentary","Short"],"scores":[23,25,56],"duration":3.5}
+{"title":"Miss Jerry","genres":["Romance"],"scores":[3,6],"duration":5.3}
+{"title":"Corbett and Courtney Before the Kinetograph","genres":["Short","Sport"],"scores":[203,250]}
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
 use std::collections::HashSet;
-use std::error::Error;
+use std::io;
 use std::io::Write;
-use std::{env, io};
 
 use json::{object::Object, JsonValue};
+use structopt::StructOpt;
 
-fn try_main() -> Result<(), Box<dyn Error>> {
-    let array_fields: HashSet<_> = env::args().collect();
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "csv2ndjson-lite",
+    about = "A little tool to convert a csv to a valid ndjson/json-stream."
+)]
+struct Opt {
+    /// The list of fields to consider as comma separated values and output as arrays.
+    #[structopt(long = "arrays", multiple = true)]
+    array_fields: Vec<String>,
+
+    /// The list of fields to consider as numbers.
+    #[structopt(long = "numbers", multiple = true)]
+    number_fields: Vec<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
+    let array_fields: HashSet<_> = opt.array_fields.into_iter().collect();
+    let number_fields: HashSet<_> = opt.number_fields.into_iter().collect();
 
     let mut wtr = io::stdout();
     let mut rdr = csv::Reader::from_reader(io::stdin());
     let headers = rdr.headers()?.clone();
+
+    for field in array_fields.r#union(&number_fields) {
+        if !headers.iter().any(|f| f == field) {
+            eprintln!("warning: field {:?} doesn't exist in the input CSV file", field);
+        }
+    }
 
     for result in rdr.into_records() {
         let record = result?;
@@ -18,8 +41,24 @@ fn try_main() -> Result<(), Box<dyn Error>> {
 
         for (i, field) in record.iter().enumerate() {
             let header = headers.get(i).unwrap();
-            let value = if array_fields.contains(header) {
-                JsonValue::Array(field.split(',').map(JsonValue::from).collect())
+            let is_number = number_fields.contains(header);
+            let is_array = array_fields.contains(header);
+            let value = if is_array {
+                let array = if is_number {
+                    field
+                        .split(',')
+                        .filter_map(|text| text.trim().parse::<f64>().ok())
+                        .map(JsonValue::from)
+                        .collect()
+                } else {
+                    field.split(',').map(JsonValue::from).collect()
+                };
+                JsonValue::Array(array)
+            } else if is_number {
+                match field.parse::<f64>() {
+                    Ok(number) => JsonValue::from(number),
+                    Err(_) => continue,
+                }
             } else {
                 json::from(field)
             };
@@ -32,10 +71,4 @@ fn try_main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-fn main() {
-    if let Err(e) = try_main() {
-        eprintln!("{}", e);
-    }
 }
